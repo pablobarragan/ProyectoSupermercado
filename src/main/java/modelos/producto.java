3347abcbbfeace931af2700caf549f0ea86e8fcf
pablobarragan <pbarragandelossantos@safareyes.es>
2022-01-25 20:15:40 +0100

    inicial comit

---
+++ src/main/java/modelos/producto.java
@@ -0,0 +1,17 @@
+package modelos;
+
+public class producto {
+
+        int identificador;
+        string codigo;
+        string  descripcion;
+        LocalDate fechaCaducidad;
+
+ }
+public class almacen {
+
+    int identificador;
+    string nombre;
+    int capacidad ;
+
+}
